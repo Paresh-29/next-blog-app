24d6756d43907509906be47e0dade73d9c0c3039
Paresh-29 <pareshkumarbarick99@gmail.com>
2024-12-12 19:16:43 +0530

    fix &apos;

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="self-center py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Paresh's
+          Paresh&apos;s
         </span>
         Blog
       </Link>
